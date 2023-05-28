cb1770606e1b2519fdff9082e1308adafe89dd0b
sellayunda <134845239+sellayunda@users.noreply.github.com>
2023-05-28 18:39:56 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from http import client
+from flask import Flask, render_template, request, jsonify, escape
+from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DB_HOST = os.environ.get("MONGODB_URL")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(DB_HOST)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# route untuk memasukan data ke database
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg': 'data berhasil ditambahkan'})
+
+# route untuk mengambil data
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'message': message_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
